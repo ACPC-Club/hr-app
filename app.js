728811ab38e1f8c71e29f76c03ddc14bd01ae203
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2024-08-06 08:25:44 +0300

    created the login page

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const path = require("path");
+const favicon = require("serve-favicon");
+
+require("dotenv").config();
+const { connectToMongoDB } = require("./config/mongo.js");
+const { setupRoutes } = require("./routes/routes.js");
+
+const app = express();
+// Serve static files
+app.use(express.static("public", { maxAge: "7d" }));
+
+// Serve favicon
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set view engine
+app.set("view engine", "ejs");
+
+// Connect to MongoDB
+connectToMongoDB();
+
+// Express session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Setup routes
+setupRoutes(app);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+// Import your routes here...
+const indexRoutes = require("./indexRoutes");
+
+function setupRoutes(app) {
+  // Initialize your routes here...
+  app.use("/", indexRoutes);
+
+  // set up 404
+
+  app.use((req, res) => {
+    res.render("404", {
+      currentPage: "404",
+      user: req.session.user === undefined ? "" : req.session.user,
+    });
+  });
+}
+
+module.exports = { setupRoutes };
